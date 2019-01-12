e7d8c4db2907aac061f60c7012729441b9b18252
pavel <zhpv07@gmail.com>
2019-01-12 12:45:25 -0500

    Added migrations

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import UserInfo, Post
 
-# Register your models here.
+class UserInfoInLine(admin.StackedInline):
+    model = UserInfo
+    fields = ['phone', 'avatar']
+
+class UserInfoAdmin(UserAdmin):
+    inlines = [UserInfoInLine]
+
+admin.site.unregister(User)
+admin.site.register(User, UserInfoAdmin)
+admin.site.register(Post)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class UserInfo(models.Model):
+    name = models.OneToOneField(User, on_delete=True, related_name='user', verbose_name='Пользователяь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    friends = models.ManyToManyField(User, blank=True, related_name='friend', verbose_name='Друзья')
+    avatar = models.ImageField(null=True, blank=True, verbose_name='Фотография')
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='author', verbose_name='Автор', on_delete=models.PROTECT)
+    title = models.TextField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    public_data = models.DateTimeField(default=timezone.now)
